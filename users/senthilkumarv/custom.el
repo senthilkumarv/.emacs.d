69f9d46c29d5d01e88bbe0d7ca35b497c839a0d4
Senthil Kumar <senthilkumar@thoughtworks.com>
2013-05-26 03:22:15 +0530

    Add mac pasteboard intergration

--- users/senthilkumarv/custom.el
+++ users/senthilkumarv/custom.el
@@ -2,3 +2,15 @@
 (global-linum-mode)
 ;; Added to introduce space after line number in linum-mode
 (setq linum-format "%d ")
+;;Pasteboard integration
+(defun mac-copy ()
+  (shell-command-to-string "pbpaste"))
+
+(defun mac-paste (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'mac-paste)
+(setq interprogram-paste-function 'mac-copy)
