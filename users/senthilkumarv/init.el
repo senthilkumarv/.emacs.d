48e1395f61d103718274fd762c85e2ff031a3f4a
Senthil Kumar <senthilkumar@thoughtworks.com>
2013-05-26 03:20:24 +0530

    Install additional packages

---
+++ users/senthilkumarv/init.el
@@ -0,0 +1,17 @@
+;; Install extensions if they're missing
+(defun init--additional-install-packages ()
+  (packages-install
+   '(exec-path-from-shell
+     haskell-mode
+     scala-mode2
+     auto-complete
+     popup)))
+
+
+(condition-case nil
+    (init--additional-install-packages)
+  (error
+   (package-refresh-contents)
+   (init--additional-install-packages)))
+
+(require 'my-key-bindings)
