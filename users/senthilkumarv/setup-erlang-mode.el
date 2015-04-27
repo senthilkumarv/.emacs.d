0c4d0ed52a6b1458df0e2a26dd9fa877815535f4
Senthil Kumar <senthil.vijay.kumar@gmail.com>
2015-04-26 23:04:53 -0400

    Update erlang version - Senthil

--- users/senthilkumarv/setup-erlang-mode.el
+++ users/senthilkumarv/setup-erlang-mode.el
@@ -1,4 +1,4 @@
-(setq load-path (cons  "/usr/local/lib/erlang/lib/tools-2.6.14/emacs"
+(setq load-path (cons  "/usr/local/lib/erlang/lib/tools-2.7.1/emacs"
                        load-path))
 (setq erlang-root-dir "/usr/local/lib/erlang")
 (setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
